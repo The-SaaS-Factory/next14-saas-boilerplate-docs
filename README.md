9d2d11f5d67df11e7b9f5715a6f289c0930a60fd
Royler Marichal Carrazana <cluzstudio@gmail.com>
2024-04-16 21:35:49 -0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# The SaaS Factory Boilerplate Docs
+
+This repository contains the documentation for the SaaS Factory Boilerplate.
+
+Link: [https://docs.thesaasfactory.dev](https://docs.thesaasfactory.dev)
---
+++ theme.config.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <img src="https://thesaasfactory.dev/_next/image?url=%2Fassets%2Fimg%2Flogo.png&w=128&q=75" alt="" />,
+  project: {
+    link: 'https://github.com/The-SaaS-Factory/frontend-saas-boilerplate-react-apollo-graphql-TS',
+  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/The-SaaS-Factory/frontend-saas-boilerplate-react-apollo-graphql-TS',
+  footer: {
+    text: 'The SaaS Factory Boilerplate Docs ',
+  },
+}
+
+ 
+
+export default config
